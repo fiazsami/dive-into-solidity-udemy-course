e1a3bdab1af804f3b8a5b88a638fbb901c6539e0
Fiaz Sami <fiaz.sami@gmail.com>
2023-01-28 23:43:57 +0000

    completes KnowledgeTest.sol

--- contracts/KnowledgeTest.sol
+++ contracts/KnowledgeTest.sol
@@ -6,8 +6,40 @@ contract KnowledgeTest {
     string[] public tokens = ["BTC", "ETH"];
     address[] public players;
 
-    function changeTokens() public view {
-        string[] memory t = tokens;
-        t[0] = "VET";
+    address public owner;
+
+    modifier onlyOwner() virtual {
+        require(msg.sender == owner, "ONLY_OWNER");
+        _;
     }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    receive() external payable{}
+
+    function getBalance() external view returns (uint) {
+        return address(this).balance;
+    }
+
+    function transferAll(address payable _toAddress) external onlyOwner returns (bool) {
+        uint balance = address(this).balance;
+        (bool success, ) = _toAddress.call{value: balance}("");
+        return success;
+    }
+
+    function changeTokens() public onlyOwner {
+        tokens[0] = "VET";
+    }
+
+    function start() public {
+        players.push(msg.sender);
+    }
+
+    function concatenate(string memory str1, string memory str2) external pure returns (string memory) {
+           return string.concat(str1,str2);
+    }
+
 }
+
