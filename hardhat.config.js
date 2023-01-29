102a434618fe57348cb05a244cfb7bd926dabe48
Fiaz Sami <fiaz.sami@gmail.com>
2023-01-29 07:33:28 +0000

    added gas estimator

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,12 @@
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.13",
+    solidity: "0.8.13",
+    gasReporter: {
+        currency: "USD",
+        gasPrice: 15,
+    },
 };
